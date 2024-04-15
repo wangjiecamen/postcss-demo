1f0f5ecefc3bb0061af56273658874a460b38d66
wangjie <1009602319@qq.com>
2024-04-15 15:53:47 +0800

    init

--- postcss-demo/src/webcomponent/web/p-a51295f7.entry.js
+++
@@ -1,2 +0,0 @@
-import{r as d,h as e}from"./p-2446730d.js";const s=":host{display:block}div{width:100px}";const t=s;const n=class{constructor(e){d(this,e);this.first=undefined;this.middle=undefined;this.last=undefined}render(){return e("div",{key:"823bb90d22d599642789de7d4f92d6e9e8e9c614"},"Hello, World")}};n.style=t;export{n as my_component};
-//# sourceMappingURL=p-a51295f7.entry.js.map
